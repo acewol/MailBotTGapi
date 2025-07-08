1c84391037a4413dd3b41b5b5f9bb71682f3bb32
acewol <shura.evstigneev@list.ru>
2025-07-08 12:13:42 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import email
+import imaplib
+import time
+from telegram import Bot
+
+# Конфигурация
+TOKEN = ""
+CHAT_ID = ""
+EMAIL = ""
+PASSWORD = ""
+IMAP_SERVER = ""
+
+bot = Bot(token=TOKEN)
+
+def fetch_emails():
+    mail = imaplib.IMAP4_SSL(IMAP_SERVER)
+    mail.login(EMAIL, PASSWORD)
+    mail.select("inbox")
+
+    status, message = mail.search(None, "(UNSEEN)")
+    if status != "OK":
+        return
+
+
+    email_ids = message[0].split()
+    for e_id in email_ids:
+        _, msg_data = mail.fetch(e_id, "(RFC822)")
+        raw_email = msg_data[0][1]
+        msg = email.message_from_bytes(raw_email)
+        
